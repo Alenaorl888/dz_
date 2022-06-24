4304ecc940ed4d6c30bf28ecd1b99ea1003e18f0
orlova <lenaorlova@example.com>
2022-06-25 00:48:51 +0300

    exz003

---
+++ exzampl003/Program.cs
@@ -0,0 +1,16 @@
+﻿// задача 2
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+ Console.Write ("Введите второе число");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+ if (a > b) 
+ {
+    Console.WriteLine("{0} наибольшее число", a);
+
+ }
+else 
+{
+    Console.WriteLine("{0} наибольшее число", b);
+
+}
