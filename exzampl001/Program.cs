699e5ecade97cec3e6d806cddfe73188425c4320
orlova <lenaorlova@example.com>
2022-06-25 14:51:23 +0300

    dz8

---
+++ exzampl001/Program.cs
@@ -0,0 +1,20 @@
+﻿// задача №8 дз
+Console.WriteLine("Введите число");
+int i = 1;
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+for (i = 1; i <= n; i++)
+{
+    if ( i % 2 ==0)
+
+    {
+        Console.WriteLine( i + "");
+ 
+    }
+}
+
